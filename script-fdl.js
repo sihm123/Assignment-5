d79693cbfe5b384b079ea772bc1cd0e26a005238
sihm123 <sihm@clemson.edu>
2025-11-10 18:52:23 -0500

    Update script-fdl.js

--- script-fdl.js
+++ script-fdl.js
@@ -1,5 +1,6 @@
 //TODO: read the dataset
-
+d3.json("miserables.json").then(function(graph){
+  
   var width = 500
   var height= 500
 
@@ -7,12 +8,17 @@ var svg =  d3.select("#fdl")
     .style("width", width)
     .style("height", height)
     
-    var nodes = ; //TODO: read from the dataset the nodes of the graph
-    var arcs = ;//TODO: read from the dataset the arcs of the graph
+    var nodes = graph.nodes; //TODO: read from the dataset the nodes of the graph
+    var arcs = graph.links ;//TODO: read from the dataset the arcs of the graph
     
-    var layout = ;//TODO: create an approprate layout using four forces: forceCenter(), forceCollide(), forceManyBody() and forceLink()
+    var layout = d3.forceSimulation(nodes)
+                   .force('center', d3.forceCenter(width/2, height/2))
+                   .force('collide', d3.forceCollide(15))
+                   .force('charge', d3.forceManyBody().strength(-50))
+                   .force('link', d3.forceLink(arcs).id(d=>d.id))
+                   .on('tick', ticked);//TODO: create an approprate layout using four forces: forceCenter(), forceCollide(), forceManyBody() and forceLink()
     
-    var color = ;//TODO: create a color scale to map on the groups of the graph nodes
+    var color = d3.scaleOrdinal(d3.schemeCategory10); ;//TODO: create a color scale to map on the groups of the graph nodes
     
     var edges = svg.append("g")
                   .selectAll("line")
@@ -27,7 +33,7 @@ var svg =  d3.select("#fdl")
                   .data(nodes)
                   .enter()
                   .append("circle")
-                  .attr("fill", /*....*/) //TODO: set the appropriate color for each node depending on its group
+                  .attr("fill", d => color(d.group)) //TODO: set the appropriate color for each node depending on its group
                   .attr("r", 10)
     
     function ticked(){
@@ -41,3 +47,4 @@ var svg =  d3.select("#fdl")
         .attr("x2", d => d.target.x)
         .attr("y2", d => d.target.y)
     }
+});
