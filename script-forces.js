5383f593962d7f6aba270835798c925d49f24bc0
sihm123 <sihm@clemson.edu>
2025-11-10 18:32:21 -0500

    Create script-forces.js

---
+++ script-forces.js
@@ -0,0 +1,31 @@
+
+var width = 500
+var height= 500
+
+var svg = d3.select("#forces")
+  .style("width", width)
+  .style("height", height)
+  
+let nodes = [{id: 1, r:10}, {id: 2, r:30}, {id: 3, r:15}, {id: 4, r:2}] 
+
+var layout = d3.forceSimulation(nodes)
+              .force('center', d3.forceCenter(width/2, height/2))
+              .force('collisions', d3.forceCollide(4))
+              .on('tick', ticked)
+
+let node = svg.append("g")
+              .selectAll("circle")
+              .data(nodes).enter()
+              .append("circle")
+              .attr('cx', d => d.x)
+              .attr('cy', d => d.y)
+              .attr("fill", "black")
+              .attr("opacity", 0.4)
+              .attr("r", d => d.r)
+            
+function ticked(){
+  svg.selectAll("circle")
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+}
+
